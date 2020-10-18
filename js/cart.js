2f8e97f5428a9fd854937f8cdd032b06e0d5e565
Fabrem <fjbremermann@gmail.com>
2020-10-18 01:45:29 -0300

    Commienzo del desafiate de la entrega 6 y primeros test del google login

--- js/cart.js
+++ js/cart.js
@@ -17,21 +17,27 @@ function showCartArray(){
                             <img src="`+ cart.src +`" alt="" class="rounded img-fluid w-100">
                         </div>
                         <div class="col-md-7 col-lg-9 col-xl-9">
-                            <div>
-                                <div class="row d-flex justify-content-between">
-                                    <div class="col-7 px-0">
-                                        <h6>`+ cart.name +`</h6>
-                                    </div>
-                                    <div class="col-2 px-0">
-                                        <div class="def-number-input number-input pl-4">
-                                            <input class="form-control quantity" min="0" name="quantity" value="`+ cart.count +`" type="number">
-                                        </div>
-                                    </div>
-                                    <div class="col-3 pl-0 text-right align-text-bottom">
-                                        <p class="mb-0 "><span><strong class="item-currency">`+ cart.currency +" "+`</strong><strong class="cart-price">`+ cart.unitCost +`</strong></span></p>
+                            <div class="row">
+                                <div class="col-7 px-0">
+                                    <h6>`+ cart.name +`</h6>
+                                </div>
+                                <div class="col-2 px-0">
+                                    <div class="pl-4">
+                                        <input class="form-control quantity" min="0" name="quantity" value="`+ cart.count +`" type="number">
                                     </div>
                                 </div>
+                                <div class="col-3 pl-0 text-right align-text-bottom">
+                                    <p class="mb-0 "><span><strong class="item-currency">`+ cart.currency +" "+`</strong><strong class="cart-price">`+ cart.unitCost +`</strong></span></p>
+                                </div>
                             </div>
+                            <button class="delete-item btn btn-danger float-right mt-5">
+                                <span>
+                                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                        <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                    </svg>
+                                </span>
+                            </button>
                         </div>
                     </div>
                 </div>
@@ -113,6 +119,17 @@ document.addEventListener("DOMContentLoaded", function(e){
 
                     updateCartTotal()
                 })
+            }
+
+            var deleteCartItemButtons = document.getElementsByClassName('delete-item')
+            for (let i = 0; i < deleteCartItemButtons.length; i++) {
+                var deleteCartItem = deleteCartItemButtons[i];
+                
+                deleteCartItem.onclick = () => {
+                    deleteButton = event.target
+                    console.log(deleteButton);
+                    deleteButton.parentElement.parentElement.parentElement.remove()
+                }
             }
         }
     });
