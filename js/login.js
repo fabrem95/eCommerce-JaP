c2ffebdb7332de78095c41dfb7738aab539bc24b
Fabrem <fjbremermann@gmail.com>
2020-10-18 02:16:32 -0300

    Mas ajustes para el login

--- js/login.js
+++ js/login.js
@@ -3,6 +3,17 @@
 //elementos HTML presentes.
 document.addEventListener("DOMContentLoaded", function(e){
 
+    document.getElementById('googleLogin').onclick = function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+    }
+    
     loginForm.onsubmit = function (e){
         e.preventDefault();
         const user = document.getElementById("email").value;
