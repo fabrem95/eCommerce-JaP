662f3d6254b935ed605dd0e14aed3a7a848e8bb4
Fabrem <fjbremermann@gmail.com>
2020-10-21 11:32:23 -0300

    Cree el boton para quitar un item del carro de compras y que el precio se vea reflejado en el subtotal, total, etc

--- js/cart.js
+++ js/cart.js
@@ -38,7 +38,10 @@ function showCartArray(){
                                 </div>
                             </div>
                             <button class="delete-item btn btn-outline-danger float-right mt-5">
-                                Quitar
+                                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path class="bi-trash" d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                                    <path class="bi-trash" fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                                </svg>
                             </button>
                         </div>
                     </div>
@@ -50,6 +53,7 @@ function showCartArray(){
 }
 
 function updateCartTotal() {
+    total = 0
 
     for (var i = 0; i < cartRows.length; i++) {
         var cartRow = cartRows[i]
@@ -60,7 +64,6 @@ function updateCartTotal() {
         var quantity = quantityElement.value
 
         if (itemCurrency === "UYU ") {
-            total = 0
             total += (price * quantity)
         } else {
             total += (price * quantity)*40
@@ -143,8 +146,9 @@ document.addEventListener("DOMContentLoaded", function(e){
                 var deleteCartItem = deleteCartItemButtons[i];
                 
                 deleteCartItem.onclick = () => {
-                    deleteButton = event.target
-                    deleteButton.parentElement.parentElement.parentElement.remove()
+                    var deleteButton = event.target
+
+                    deleteButton.closest('.card-body').remove();
 
                     updateCartTotal()
                 }
